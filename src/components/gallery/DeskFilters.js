a47a7185ed473915a6d51cc59919769699fce28c
Rishabh <rishabh@Rishabhs-MacBook-Pro.local>
2024-03-28 16:58:56 +0530

    changes

--- src/components/gallery/DeskFilters.js
+++ src/components/gallery/DeskFilters.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './Gallery.css';
 
 const DeskFilters = ({ arrFilters, handleFilterClose, clearAllFilters, handleIndividualCheckboxChange, selectedFilter, expand, expandDiv, filterState }) => {
